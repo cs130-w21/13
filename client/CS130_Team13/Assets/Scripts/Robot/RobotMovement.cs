04c0da778df2ee2289f0fbcb3486380a7e078de8
markgue <32917194+markgue@users.noreply.github.com>
2021-02-10 22:04:53 -0800

    More skeleton code in robot movement
    Added helper Direction enum and function that will be helpful later

--- client/CS130_Team13/Assets/Scripts/Robot/RobotMovement.cs
+++ client/CS130_Team13/Assets/Scripts/Robot/RobotMovement.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+Vector2 GetDirectionVector(Direction dir)
+{
+    switch (dir)
+    {
+        case Direction.Up: 
+            return Vector2.up;
+        case Direction.Down: 
+            return Vector2.down;
+        case Direction.Left: 
+            return Vector2.left;
+        case Direction.Right: 
+            return Vector2.right;
+        default: 
+            return Vector2.zero;
+    }
+}
+enum Direction {
+Up, Down, Left, Right
+}
+
+
 public class RobotMovement : MonoBehaviour
 {
 
     void ProcessCommands(string cmdstr)
     {
+            //Take in a string and parse it for movement directions
+    }
 
+    void Move(Direction dir)
+    {
+        
+    }
+
+    void PlaceRock(Direction dir)
+    {
+        
     }
 
 
 
 }
+
