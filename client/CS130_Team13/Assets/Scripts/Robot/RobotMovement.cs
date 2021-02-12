28321987fd45e697875b337981aec955b244b6b1
markgue <32917194+markgue@users.noreply.github.com>
2021-02-11 20:16:41 -0800

    Implemented robot movement
    Demo is in MarkWorkspace. Use WASD to move. Currently a little buggy since the program won't be expecting async inputs from the player when fully implemented later. Tap WASD once and wait until it completes moving.

--- client/CS130_Team13/Assets/Scripts/Robot/RobotMovement.cs
+++ client/CS130_Team13/Assets/Scripts/Robot/RobotMovement.cs
@@ -2,46 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-Vector2 GetDirectionVector(Direction dir)
-{
-    switch (dir)
-    {
-        case Direction.Up: 
-            return Vector2.up;
-        case Direction.Down: 
-            return Vector2.down;
-        case Direction.Left: 
-            return Vector2.left;
-        case Direction.Right: 
-            return Vector2.right;
-        default: 
-            return Vector2.zero;
-    }
-}
-enum Direction {
-Up, Down, Left, Right
-}
 
+// abstract public class RobotCommand
+// {
+//     public abstract void RunCommand();
 
+// }
 public class RobotMovement : MonoBehaviour
 {
+    public float actionSpeed = 0.4f;
 
-    void ProcessCommands(string cmdstr)
+    // Takes in two Vector3 positions and moves the object to that position
+    IEnumerator MoveToCoroutine(Vector3 dest) 
+    {
+         Vector3 start = transform.position;
+         float timer = 0;
+         while (timer <= actionSpeed) 
+         {
+             timer += Time.deltaTime;
+             transform.position = Vector3.Lerp(start, dest, timer/actionSpeed);
+             yield return null;
+         }
+         transform.position = dest;
+ }
+
+    public void ProcessCommands(string cmdstr)
     {
             //Take in a string and parse it for movement directions
     }
 
-    void Move(Direction dir)
+    public void Move(Vector3 dir, float dist)
+    {
+        /*if (CheckMoveCollision() == true)
+        {
+            return;
+        }*/
+        print("Starting Move");
+        StartCoroutine(MoveToCoroutine(transform.position + dir.normalized * dist));
+    }
+
+    public void PlaceRock(Vector3 dir)
     {
         
     }
-
-    void PlaceRock(Direction dir)
-    {
-        
-    }
-
-
-
 }
 
---
+++ client/CS130_Team13/Assets/Scripts/Robot/RobotTestController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotTestController : MonoBehaviour
+{
+    public GameObject robot;
+    private RobotMovement robotMovement; 
+    private bool isMoving;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotMovement = robot.GetComponent<RobotMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+        {
+            isMoving = true;
+            robotMovement.Move(Vector3.up, 1);
+            isMoving = false;
+        } 
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            isMoving = true;
+            robotMovement.Move(Vector3.left, 1);
+            isMoving = false;
+        } 
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            isMoving = true;
+            robotMovement.Move(Vector3.down, 1);
+            isMoving = false;
+        } 
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            isMoving = true;
+            robotMovement.Move(Vector3.right, 1);
+            isMoving = false;
+        } 
+        }
+
+    }
+}
