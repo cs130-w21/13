c6260caeab9f44752329dac1fd0cddf3bec7995f
markgue <32917194+markgue@users.noreply.github.com>
2021-02-20 18:24:34 -0800

    Start of BoardManager
    Can generate a board based on width/height

---
+++ client/CS130_Team13/Assets/Scripts/Board/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public BoardManager boardManager;
+    void Start()
+    {
+        int seed = (int)Random.Range(0,100);
+        boardManager.CreateBoard(seed);
+    }
+}
