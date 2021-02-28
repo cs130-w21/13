47652b0b334f488adeed97dfa8b1fa66d962e852
Andy Peng <andypg20@gmail.com>
2021-02-28 06:18:07 -0800

    add abstraction layer

--- client/CS130_Team13/Assets/Scripts/RemoteController.cs
+++ client/CS130_Team13/Assets/Scripts/RemoteController.cs
@@ -71,4 +71,65 @@ public class RemoteController : MonoBehaviour {
         Debug.Log("byeee");
         socket.Disconnect();
     }
+
+    // --------------------------------------------------------------------------------
+    // The following functions should not do anything other than call other functions
+    // present elsewhere in the code. They exist as an abstraction layer between
+    // server and client: if the implementation of one is changed, then you'll have
+    // to change some function names here -- but the benefit is that you _won't_ have
+    // to go digging through the rest of the code to find all the references in the
+    // other half of the project.
+    // --------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Connect to the server and tell it to match the player with an opponent.
+    ///
+    /// This should be called by C# functions within Unity, and it should call
+    /// Socket.IO-related functions within RemoteController.
+    /// </summary>
+    public void SearchForGame() {
+        // TODO
+    }
+
+    /// <summary>
+    /// Start a new game.
+    ///
+    /// This should be called by the Socket.IO-related functions within RemoteController,
+    /// and it should call C# functions within Unity.
+    /// </summary>
+    public void GameFound() {
+        // TODO
+    }
+
+    /// <summary>
+    /// Send the current player's command inputs to the server.
+    ///
+    /// This should be called by C# functions within Unity, and it should call
+    /// Socket.IO-related functions within RemoteController.
+    /// </summary>
+    /// <param name="commands">the player's commands, to be sent to the server</param>
+    public void SendPlayerCommands(string commands) {
+        // TODO
+    }
+
+    /// <summary>
+    /// Process the opponent's commands.
+    ///
+    /// This should be called by the Socket.IO-related functions within RemoteController,
+    /// and it should call C# functions within Unity.
+    /// </summary>
+    /// <param name="commands">the opponent's commands, received from the server</param>
+    public void ReceiveOpponentCommands(string commands) {
+        // TODO
+    }
+
+    /// <summary>
+    /// End the current game.
+    ///
+    /// This should be called by the Socket.IO-related functions within RemoteController,
+    /// and it should call C# functions within Unity.
+    /// </summary>
+    public void GameEnded() {
+        // TODO
+    }
 }
