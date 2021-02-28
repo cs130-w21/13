2b39c09235d06b0fa73166da0cc96be67c7b208d
Regina Wang <rrreggginnna@gmail.com>
2021-02-28 15:25:06 -0800

    port to env port if given

--- server/index.ts
+++ server/index.ts
@@ -93,6 +93,7 @@ io.on('connection', (socket) => {
 /**
  * Listen on port 3000
  */
-http.listen(3000, () => {
-  console.log('Connected at 3000');
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log('Connected at ' + port);
 });
