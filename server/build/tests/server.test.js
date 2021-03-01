2c4446f9d1d12cf92ec5f3746b0ee4c331d98cb8
Ziyi Peng <andypg20@gmail.com>
2021-03-01 10:12:56 -0800

    Server testing (#62)
    * add test dependencies
    
    * additional typescript dependencies
    
    * move server to server.ts
    
    * add test file
    
    * modify autodoc generation to accomodate refactoring
    
    * remove unnecessary unix advisory
    
    * modify readme
    
    * Add server CICD to pipeline (#65)
    
    * add cicd
    
    * change nodemon prod to tsnode prod
    
    * try fix build
    
    * add tsc
    
    * fix require error
    
    * fix tsc
    
    * remove
    
    * npm install
    :
    
    * heroku fix
    
    * heroku fix
    
    * typescript import
    
    * run compiled js
    
    * put back command
    
    * accidentally removed tsc command
    
    * port to env port if given
    
    * add docs
    
    * fix typo
    
    * update config site link
    
    * Basic player movement (#66)
    
    * Basic scene and framework for robot implementation
    
    * More skeleton code in robot movement
    
    Added helper Direction enum and function that will be helpful later
    
    * Implemented robot movement
    
    Demo is in MarkWorkspace. Use WASD to move. Currently a little buggy since the program won't be expecting async inputs from the player when fully implemented later. Tap WASD once and wait until it completes moving.
    
    * Start of BoardManager
    
    Can generate a board based on width/height
    
    * Create a board of arbitrary size
    
    * Places players at opposite corners
    
    Player 1 in bottom left, Player 2 in top right
    
    * Added doxygen plugin to Unity + generated documentation
    
    Doxygen plugin allows us to generate Doxygen documentation for the entire Unity project easily.
    
    * Client autodocumentation updated to current master branch
    
    * Added movement functionality for the robots
    
    The board can now move the player robots around
    
    * Client abstraction layer (#63)
    
    * change remotecontroller indentation style
    
    * add abstraction layer
    
    * changes from peer review
    
    * fix bugs
    
    * fix CI test script
    
    * update heroku api key
    
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>
    Co-authored-by: Mark Guevara <32917194+markgue@users.noreply.github.com>

---
+++ server/build/tests/server.test.js
@@ -0,0 +1,47 @@
+"use strict";
+/**
+ * This is the test file corresponding to ../server.ts.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const { http, io } = require("../server.ts");
+const Client = require("socket.io-client");
+const assert = require("chai").assert;
+/******************************************************************************/
+describe("server-socket-test", () => {
+    let clientSocket1, clientSocket2;
+    let url = 'http://localhost:3000';
+    /**
+     * Initialize server, client sockets
+     */
+    before((done) => {
+        http.listen(3000, () => {
+            clientSocket1 = new Client(url);
+            clientSocket1.on("connect", () => {
+                clientSocket2 = new Client(url);
+                clientSocket2.on("connect", done);
+            });
+        });
+    });
+    /**
+     * Teardown sockets
+     */
+    after(() => {
+        io.close();
+        clientSocket1.close();
+        clientSocket2.close();
+    });
+    /**
+     * Tests whether two clients can connect to the server.
+     */
+    it("hello-test-two-clients", (done) => {
+        clientSocket1.emit('hello', '{"name":"client1","id":1}');
+        clientSocket1.on('initiate', (msg) => {
+            assert.equal(msg, 'you are user 1!');
+            clientSocket2.emit('hello', '{"name":"client2","id":2}');
+            clientSocket2.on('initiate', (msg) => {
+                assert.equal(msg, 'you are user 2!');
+                done();
+            });
+        });
+    });
+});
