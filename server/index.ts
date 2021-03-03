a6cde402360385a1abbe889bccf2b713ca16c80c
Regina Wang <rrreggginnna@gmail.com>
2021-03-02 20:47:41 -0800

    ease client side work when server not fully setup (#69)
    * easier for client side work to work when server not finished being setup"
    
    * test deploy
    
    * change object name and error check server request properly
    
    * remove thing that'll cause bugs

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,6 @@ const { http, io } = require("./server.ts");
 const port = process.env.PORT || 3000;
 http.listen(port, () => {
   console.log('Connected at ' + port);
-});
+}); 
 
 export {}  // suppress TS import checker
