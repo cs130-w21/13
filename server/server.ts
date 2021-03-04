3c37d24ae64be94e9b901c030bd69a34a7079c05
ashen-00 <61583371+ashen-00@users.noreply.github.com>
2021-03-04 14:50:18 -0800

    Server Code Done? (#72)
    * Server Code Done?
    
    * Update server.test.ts
    
    Co-authored-by: asoo <61583371+asoo-00@users.noreply.github.com>
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -32,26 +32,35 @@ class UserProps {
   name!: string;
   id!: number;
   socketId!: string;
+  commands!: string;
 
   constructor(name: string, id: number, socketId: string) {
     this.name = name;
     this.id = id;
     this.socketId = socketId;
+    this.commands = "*";
   }
 }
 
-var user1: UserProps;
-var user2: UserProps;
+let user1: UserProps;
+let user2: UserProps;
 
 /******************************************************************************/
 
 /**
+ * Server waits for both players to connect, and sends a "gameplay" event to both
+ *  once 2 players have connected
+ * Additional players will receive the error event "initiate"
  * Ready to listen for others (imagine that it is a function for one connection)
- * TODO: switch to socket.id use
+ * When initializing the game, also initialize state
+ *  0 : no commands submitted, 1: P1 only submitted, 2: P2 only submitted, 3: both submitted
  */
+let current_state;  
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  // The "hello" event is how a user initially connects to the server
+  //  msg should contain a userInfo object
   socket.on('hello', (msg) => { // listening for event `hello`
     console.log("we received a message: " + msg);
     msg = JSON.parse(msg);
@@ -59,25 +68,71 @@ io.on('connection', (socket) => {
     // if user is first user, setup
     if (user1 === undefined || user1.id === msg.id) { 
       user1 = new UserProps(msg.name, msg.id, socket.id);
-      socket.emit("initiate", 'you are user 1!');
+      // Returns 1 to indicate player 1
+      socket.emit("initiate", 1);
 
     // if user is second user, setup and allow for starting game by telling clients
     } else if (user2 === undefined || user2.id === msg.id) {
       user2 = new UserProps(msg.name, msg.id, socket.id);
-      socket.emit("initiate", 'you are user 2!');
+      // Returns 2 to indicate player 2
+      socket.emit("initiate", 2);
 
       socket.emit("gameplay", user1 );
       socket.to(user1.socketId).emit("gameplay", user2 ); // sends message to specific client based on clientId
+      current_state = 0;
 
     // for excess users, cya 
     } else {
-      socket.emit("initiate", 'no space for u :( byebye try again later');
+      socket.emit("initiate", 'There are already 2 players. Please try again later.');
     }
   });
 
-  // can have other socket events ons
-  socket.on('gameplay', () => {
-  })
+  /**
+  * Receives turn info from users, and sends turn info to other player once both
+  *   players have submitted
+  * Recall that for current_state:
+  *   0 : no commands submitted, 1: P1 only submitted, 2: P2 only submitted, 3: both submitted
+  */
+  socket.on('submittingTurn', (msg) => {
+    //console.log("Server has received commands from a user");
+    msg = JSON.parse(msg);
+
+    // Check which user has submitted and change state accordingly
+    if(user1.id == msg.playerId){
+      user1.commands = msg.commandString;
+      if(current_state == 0 || current_state == 2) {
+        current_state++;
+      }  
+      console.log("Received commands from player 1");
+      console.log("Current State: " + current_state);
+    }
+    else if(user2.id == msg.playerId){
+      user2.commands = msg.commandString;
+      if(current_state == 0 || current_state == 1) {
+        current_state += 2;
+      }  
+      console.log("Received commands from player 2");
+      console.log("Current State: " + current_state);
+    }
+
+    // If we've received both sets of commands, send back to players
+    if(current_state == 3) {
+      socket.to(user1.socketId).emit("receiveTurn", user2.commands);
+      socket.to(user2.socketId).emit("receiveTurn", user1.commands);
+      console.log("Sending commands to both players")
+      current_state = 0;
+    }
+  });
+
+  /**
+  * When server receives a end game signal from either client, send both clients
+  *   an end game message
+  */
+  socket.on('endGameRequest', () => { 
+    console.log('Server received an end game request');
+    socket.to(user1.socketId).emit("endGameConfirmation", "");
+    socket.to(user2.socketId).emit("endGameConfirmation", "");
+  });
 
   // this method like socket will fire when user disconnects
   socket.on('disconnect', () => { 
