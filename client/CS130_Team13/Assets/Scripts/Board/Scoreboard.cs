39a686cfb688447555c2e18d3b8d6ccd23637b3f
Mark Guevara <32917194+markgue@users.noreply.github.com>
2021-03-07 15:28:21 -0800

    Tweaked gameplay loop and added scoreboard UI (#89)
    * Semi-functional Gameplay loop + UI
    
    Still needs a few tweaks
    
    * Merged in with server code, now semi-functional
    
    * Debugging network stuff
    
    server currently runs client side
    
    * Added a bunch of prefabs / made the UI pretty
    
    * destroy better
    
    * fix errors
    
    * fix tests
    
    * client side fixes
    
    * switch server url
    
    * Tweaked GameManager
    
    * Added scoreboard
    
    * Updated scene slightly
    
    Co-authored-by: Regina Wang <rrreggginnna@gmail.com>

---
+++ client/CS130_Team13/Assets/Scripts/Board/Scoreboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text p1;
+    public Text p2;
+    public Text timer;
+
+    public GameManager gameManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        p1.text = gameManager.getP1Score().ToString();
+        p2.text = gameManager.getP2Score().ToString();
+        timer.text = Mathf.Max(0,Mathf.Ceil(gameManager.getTimeRemaining())).ToString();
+    }
+}
