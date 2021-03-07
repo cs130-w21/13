f85b80394a98217855e8896765d542a708e75cf9
Xiangyu Wan <39484269+JohnnyXiangyu@users.noreply.github.com>
2021-03-06 23:53:40 -0800

    Scene management (#83)
    * created SceneLoader script
    
    * Imported TMP and created template opening scene
    
    * created template settings system

---
+++ client/CS130_Team13/Assets/Scripts/Menus/SettingsHub.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsHub : MonoBehaviour
+{
+    public static SettingsHub instance = null;
+
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private float currentVolume = 0;
+    
+    public void UpdateVolume(float newValue) {
+
+    }
+
+    /// <summary>
+    /// Return the current volume percentage (0 - 1)
+    /// </summary>
+    /// <returns>float: the percentage of max volume</returns>
+    public float GetVolume() {
+        return currentVolume;
+    }
+}
