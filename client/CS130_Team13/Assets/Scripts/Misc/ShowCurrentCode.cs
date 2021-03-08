6214802c2ae98a814f50ddca02a84fc83ac1700b
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-03-07 18:44:20 -0800

    fixed bug with for loops
    the added class is not used anywhere other than my workspace scene

---
+++ client/CS130_Team13/Assets/Scripts/Misc/ShowCurrentCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowCurrentCode : MonoBehaviour
+{
+    public CodingPanel myPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<TextMeshProUGUI>().text = myPanel.GetInformation();
+    }
+}
